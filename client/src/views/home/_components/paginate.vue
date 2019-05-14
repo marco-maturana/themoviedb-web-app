a786e54148e22e16dc61ac085453ce9936cfc0cf
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2019-05-14 15:17:08 -0400

    added pagination in movie list

---
+++ client/src/views/home/_components/paginate.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-pagination
+    :length="pages"
+    :total-visible="5"
+    v-model="page"
+    @input="upcoming"
+  ></v-pagination>
+</template>
+
+<script>
+import { createNamespacedHelpers } from "vuex";
+
+const STORE_KEY = "$_VIEWS_HOME";
+const { mapActions, mapState } = createNamespacedHelpers(STORE_KEY);
+
+export default {
+  name: "ViewsHomeComponentsPaginate",
+  computed: {
+    ...mapState(["pages"]),
+    page: {
+      get () {
+        return this.$store.state[STORE_KEY].page;
+      },
+      set (value) {
+        this.$store.commit(`${STORE_KEY}/page`, value);
+      }
+    }
+  },
+  methods: {
+    ...mapActions(["upcoming"])
+  }
+};
+</script>
--- client/src/views/home/index.vue
+++ client/src/views/home/index.vue
@@ -1,5 +1,10 @@
 <template>
   <v-layout justify-space-around fluid wrap>
+    <v-flex xs12>
+      <v-layout justify-center>
+        <paginate/>
+      </v-layout>
+    </v-flex>
     <v-flex v-for="movie in movies" :key="movie.id" xs12 sm6 md4 lg3 pa-3>
       <movie-card
         :genres="movie.genres"
@@ -9,12 +14,18 @@
         :title="movie.title"
       ></movie-card>
     </v-flex>
+    <v-flex xs12>
+      <v-layout justify-center>
+        <paginate/>
+      </v-layout>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
 import store from "./_store";
 import MovieCard from "./_components/movie-card";
+import Paginate from "./_components/paginate";
 import { createNamespacedHelpers } from "vuex";
 
 const STORE_KEY = "$_VIEWS_HOME";
@@ -23,7 +34,8 @@ const { mapActions, mapState } = createNamespacedHelpers(STORE_KEY);
 export default {
   name: "ViewsHome",
   components: {
-    MovieCard
+    MovieCard,
+    Paginate
   },
   computed: {
     ...mapState(["movies"])
