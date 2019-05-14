927ffd6b0c8351aba497de28f90f03329e79ba77
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2019-05-14 07:59:07 -0400

    test configuration

---
+++ server/tests/specs/example.test.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+
+describe("test example", function () {
+  it("first test", async function () {
+      expect(0).to.equal(0);
+  });
+});
---
+++ server/tests/specs/index.test.ts
@@ -0,0 +1,6 @@
+
+import server from "@config/server";
+
+after("after", function () {
+  server.stop();
+});
