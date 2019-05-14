831d121fda3bda41bbc964e031295c9206359093
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2019-05-14 14:59:47 -0400

    created component movie-card for movie listing

---
+++ client/src/views/home/_components/movie-card.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-card class="views-home-components-movie-card">
+    <v-layout align-start justify-start row fill-height wrap>
+      <v-flex xs12>
+        <v-layout align-center justify-center>
+          <img
+            class="movie-image"
+            :src="`https://image.tmdb.org/t/p/w370_and_h556_bestv2/${posterPath}`"
+          />
+        </v-layout>
+      </v-flex>
+      <v-flex xs12 pa-2>
+        <v-layout align-space-around justify-space-between column fill-height>
+          <v-flex xs12 >
+            <div class="movie-title">
+                <div class="subheading font-weight-bold">{{title}}</div>
+                <div class="body-2 grey--text">{{releaseDate}}</div>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex xs12>
+        <v-layout align-end justify-end row fill-height>
+          <v-btn flat color="primary">
+            <v-icon left dark>remove_red_eye</v-icon>
+            More Info
+          </v-btn>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ViewsHomeComponentsMovieCard",
+  props: {
+    genres: {
+      type: Array,
+      required: true
+    },
+    id: {
+      type: Number,
+      required: true
+    },
+    posterPath: String,
+    releaseDate: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+  .views-home-components-movie-card
+    .movie-image
+      height: 200px;
+
+    .movie-title
+      height: 65px
+</style>
--- client/src/views/home/index.vue
+++ client/src/views/home/index.vue
@@ -1,18 +1,41 @@
 <template>
   <v-layout justify-space-around fluid wrap>
-    Test
+    <v-flex v-for="movie in movies" :key="movie.id" xs12 sm6 md4 lg3 pa-3>
+      <movie-card
+        :genres="movie.genres"
+        :id="movie.id"
+        :poster-path="movie.posterPath"
+        :release-date="movie.releaseDate"
+        :title="movie.title"
+      ></movie-card>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
 import store from "./_store";
+import MovieCard from "./_components/movie-card";
+import { createNamespacedHelpers } from "vuex";
 
 const STORE_KEY = "$_VIEWS_HOME";
+const { mapActions, mapState } = createNamespacedHelpers(STORE_KEY);
 
 export default {
   name: "ViewsHome",
+  components: {
+    MovieCard
+  },
+  computed: {
+    ...mapState(["movies"])
+  },
+  methods: {
+    ...mapActions(["upcoming"])
+  },
   beforeCreate () {
     this.$registerStore({ name: STORE_KEY, module: store });
+  },
+  mounted () {
+    this.upcoming();
   }
 };
 </script>
